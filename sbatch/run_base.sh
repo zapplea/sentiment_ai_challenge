1c3b484ea9cb7fac074fc3467319d74beebdec08
yibing <zapplea@gmail.com>
2019-02-22 11:15:30 +0800

    add

--- sbatch/run_base.sh
+++ sbatch/run_base.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=100GB
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 echo "loading"
 module load python/3.6.1
 module load tensorflow/1.12.0-py36-gpu
