913c3d9fe3f31db31556d4a3147badafc5b4ca37
yibing <zapplea@gmail.com>
2019-02-23 17:58:51 +0800

    add

--- data_process/tencentEmb.py
+++ data_process/tencentEmb.py
@@ -1,8 +1,11 @@
 def read(fname):
     with open(fname,'r') as f:
+        count = 0
         for line in f:
+            count+=1
             print(line)
-            exit()
+            if count>=2:
+                exit()
 
 if __name__ == "__main__":
     fname = "/datastore/liu121/wordEmb/tencent_cn/Tencent_AILab_ChineseEmbedding.txt"
