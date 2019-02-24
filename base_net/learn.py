4053372239939377ff06f4b5376d58b3c8406aeb
yibing <zapplea@gmail.com>
2019-02-24 23:25:00 +0800

    add

--- base_net/learn.py
+++ base_net/learn.py
@@ -2,6 +2,7 @@ from model import Model
 from senti_datafeeder import DataFeeder
 from train import SentiTrain
 import tensorflow as tf
+import argparse
 
 def main(config):
     with tf.device('/gpu:0'):
@@ -12,25 +13,31 @@ def main(config):
     train.train(model_dic)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--lr',type=int)
+    parser.add_argument('--reg',type=int)
+    args = parser.parse_args()
+    lr = [1e-4,1e-5,1e-6,1e-7]
+    reg = [1e-3,1e-4,1e-5,1e-6]
     config = {'model':{'biSRU':{'shared_layers_num':2,
                                 'separated_layers_num':3,
                                 'rnn_dim':200},
-                       'lr': 1e-5,
-                       'reg_rate':1e-4,
+                       'lr': lr[args.lr],
+                       'reg_rate':reg[args.reg],
                        "vocab_size": 266078,
                        'word_dim':200,
                        'max_sent_len':1141,
                        'attr_num':20,
                        'senti_num':3,
                        'padding_word_index':0,
-                       'clip_value':3.0},
+                       'clip_value':10.0},
               'train':{'epoch_num':20,
                        'report_filePath':'/datastore/liu121/sentidata2/report/aic_junyu',
-                       'early_stop_limit':5,
+                       'early_stop_limit':20,
                        'mod':1,
                        'sr_path':'/datastore/liu121/sentidata2/result/aic_junyu',
                        'attributes_num':20,},
               'datafeeder':{'batch_size':50,
-                            'train_data_file_path':'/datastore/liu121/sentidata2/data/aic2018_junyu/tenc_merged_train_data_trail.pkl',
-                            'test_data_file_path':'/datastore/liu121/sentidata2/data/aic2018_junyu/tenc_merged_dev_data_trail.pkl'}}
+                            'train_data_file_path':'/datastore/liu121/sentidata2/data/aic2018_junyu/tenc_merged_train_data.pkl',
+                            'test_data_file_path':'/datastore/liu121/sentidata2/data/aic2018_junyu/tenc_merged_dev_data.pkl'}}
     main(config)
---
+++ sbatch/flow.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sbatch run_bash.sh 0 1
+sbatch run_bash.sh 1 1
+sbatch run_bash.sh 2 1
+sbatch run_bash.sh 3 1
