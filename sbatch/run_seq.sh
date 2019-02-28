242db45b77557eb4f89dab45db0092a3117ecc76
yibing <zapplea@gmail.com>
2019-02-28 13:47:33 +0800

    add

--- sbatch/run_seq.sh
+++ sbatch/run_seq.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --get-user-env
-#SBATCH --job-name="elmo"
-#SBATCH --time=140:00:00
+#SBATCH --job-name="seq net"
+#SBATCH --time=72:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=100GB
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 echo "loading"
 module load python/3.6.1
 module load tensorflow/1.12.0-py36-gpu
