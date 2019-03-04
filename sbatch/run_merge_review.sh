9d73d505d9104fab778b1a9d7167edac389c239a
yibing <zapplea@gmail.com>
2019-03-04 14:21:39 +0800

    add

--- sbatch/run_merge_review.sh
+++ sbatch/run_merge_review.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="merge review"
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
-#SBATCH --mem=100GB
+#SBATCH --mem=200GB
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 echo "loading"
