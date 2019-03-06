d7be56dad30a12f488de338f65b185b341e6b325
yibing <zapplea@gmail.com>
2019-03-06 13:36:23 +0800

    add

--- base_withCharEmb/layers.py
+++ base_withCharEmb/layers.py
@@ -90,7 +90,7 @@ class Layers:
         condition = tf.equal(padding_id, X_id)
         print('condition: ',condition.get_shape())
         print(tf.where(condition, tf.zeros_like(X_id, dtype='int32'), tf.ones_like(X_id, dtype='int32')).get_shape())
-        seq_len = tf.reduce_sum(tf.where(condition, tf.zeros_like(X_id, dtype='int32'), tf.ones_like(X_id, dtype='int32')),axis=1)
+        seq_len = tf.reduce_sum(tf.where(condition, tf.zeros_like(X_id, dtype='int32'), tf.ones_like(X_id, dtype='int32')),axis=-1)
         print('==========')
         return seq_len
 
@@ -101,7 +101,7 @@ class Layers:
         print('condition: ', condition.get_shape())
         print(tf.where(condition, tf.zeros_like(char_id, dtype='int32'), tf.ones_like(char_id, dtype='int32')).get_shape())
         seq_len = tf.reduce_sum(
-            tf.where(condition, tf.zeros_like(char_id, dtype='int32'), tf.ones_like(char_id, dtype='int32')), axis=2)
+            tf.where(condition, tf.zeros_like(char_id, dtype='int32'), tf.ones_like(char_id, dtype='int32')), axis=-1)
         print('==========')
         return seq_len
 
