4cff11e75cc7e280296191a6341f08bc44cec7bf
yibing <zapplea@gmail.com>
2019-03-15 01:22:12 +0800

    add

--- sbatch/run_base_withCharEmb.sh
+++ sbatch/run_base_withCharEmb.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="baseChar_net"
 #SBATCH --time=140:00:00
 #SBATCH --nodes=1
-#SBATCH --mem=100GB
+#SBATCH --mem=200GB
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:1
