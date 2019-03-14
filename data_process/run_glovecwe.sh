977092a7449fa08d4f0adfdc991522977c231d60
yibing <zapplea@gmail.com>
2019-03-14 19:24:44 +0800

    add

--- data_process/run_glovecwe.sh
+++ data_process/run_glovecwe.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="CWEP"
 #SBATCH --time=12:00:00
 #SBATCH --nodes=1
-#SBATCH --mem=100GB
+#SBATCH --mem=200GB
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 module load python/3.6.1
