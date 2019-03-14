343be4bec1e7b70e75005e3d1c9292de184aba64
yibing <zapplea@gmail.com>
2019-03-14 14:26:32 +0800

    add

--- data_process/gendata_from_GloVeCWE_Emb.py
+++ data_process/gendata_from_GloVeCWE_Emb.py
@@ -33,8 +33,9 @@ class GenDataGloVeCWE:
         prepared_attr_label_collection = []
         prepared_senti_label_collection = []
         prepared_char_collection = []
-        for i in range(len(review_collection)):
-            review = review_collection[i]
+        # Fixed: for padded words, it also needs char
+        for j in range(len(review_collection)):
+            review = review_collection[j]
             reviewID_ls = []
             reviewCharID_ls = []
             for sentence in review:
@@ -43,6 +44,8 @@ class GenDataGloVeCWE:
                 sentence = sentence.split(' ')
                 for i in range(len(sentence)):
                     word = sentence[i]
+
+                    # Word
                     if len(list(word)) > config['corpus']['max_word_len']:
                         sentenceID_ls.append(self.word_to_id[self.config['corpus']['unknown_word']])
                     else:
@@ -51,31 +54,48 @@ class GenDataGloVeCWE:
                         dim0=self.config['corpus']['max_sentence_len']-len(sentenceID_ls)
                         patch = (np.ones(shape=(dim0,))*self.word_to_id[self.config['corpus']['padding_word']]).tolist()
                         sentenceID_ls.extend(patch)
-                    if word == self.config['corpus']['unknown_word']:
-                        sentenceCharID_ls.append(np.zeros(shape=(self.config['corpus']['max_word_len'],),dtype='int32').tolist())
+
+                    # Char
+                    if len(list(word)) > self.config['corpus']['max_word_len']:
+                        sentenceCharID_ls.append((np.ones(shape=(self.config['corpus']['max_word_len'],),dtype='int32')*self.char_to_id[self.config['corpus']['padding_char']]).tolist())
                     else:
                         charID_ls = []
                         char_ls = list(word)
                         for char in char_ls:
                             charID_ls.append(self.char_to_id[char])
+                        # pad char list
                         if len(charID_ls)<self.config['corpus']['max_word_len']:
                             shape = (self.config['corpus']['max_word_len'] - len(charID_ls),)
-                            charID_ls.extend(np.zeros(shape = shape,dtype='int32').tolist())
+                            charID_ls.extend((np.ones(shape = shape,dtype='int32')*self.char_to_id[self.config['corpus']['padding_char']]).tolist())
                         sentenceCharID_ls.append(charID_ls)
+                # pad sentence char list
+                if len(sentenceCharID_ls)<self.config['corpus']['max_sentence_len']:
+                    dim0 = self.config['corpus']['max_sentence_len'] - len(sentenceCharID_ls)
+                    dim1 = self.config['corpus']['max_word_len']
+                    sentenceCharID_ls.extend((np.ones(shape=(dim0,dim1),dtype='int32')*self.char_to_id[self.config['corpus']['padding_char']]).tolist())
                 reviewCharID_ls.append(sentenceCharID_ls)
                 reviewID_ls.append(sentenceID_ls)
+            # pad review char list
+            # pad review list
+            if len(reviewCharID_ls)<self.config['corpus']['max_review_len']:
+                dim0 = self.config['corpus']['max_review_len'] - len(reviewCharID_ls)
+                dim1 = self.config['corpus']['max_sentence_len']
+                dim2 = self.config['corpus']['max_word_len']
+                reviewCharID_ls.extend((np.ones(shape=(dim0,dim1,dim2),dtype='int32')*self.char_to_id[self.config['corpus']['padding_char']]).tolist())
+                reviewID_ls.extend((np.ones(shape=(dim0,dim1,),dtype='int32')*self.word_to_id[self.config['corpus']['padding_word']]).tolist())
+
             prepared_review_collection.append(reviewID_ls)
             prepared_char_collection.append(reviewCharID_ls)
 
-            label = label_collection[i]
+            label = label_collection[j]
             attr_label_ls = []
             senti_label_ls = []
-            for j in range(len(label)):
-                if label[j] in {1,0,-1}:
+            for i in range(len(label)):
+                if label[i] in {1,0,-1}:
                     attr_label_ls.append(1)
-                    if label[j]==1:
+                    if label[i]==1:
                         senti_label_ls.append([1,0,0])
-                    elif label[j] == 0:
+                    elif label[i] == 0:
                         senti_label_ls.append([0,1,0])
                     else:
                         senti_label_ls.append([0,0,1])
@@ -86,6 +106,69 @@ class GenDataGloVeCWE:
             prepared_senti_label_collection.append(senti_label_ls)
         return prepared_review_collection,prepared_attr_label_collection,prepared_senti_label_collection,prepared_char_collection
 
+    def merged_read_corpus(self,fname):
+        data = pd.read_pickle(fname)
+        label_collection = data[:, 2]
+        review_collection = data[:, 1]
+        prepared_review_collection = []
+        prepared_attr_label_collection = []
+        prepared_senti_label_collection = []
+        prepared_char_collection = []
+        # Fixed: for padded words, it also needs char
+        for j in range(len(review_collection)):
+            review = review_collection[j]
+            reviewID_ls = []
+            reviewCharID_ls = []
+            for sentence in review:
+                sentence = sentence.split(' ')
+                for i in range(len(sentence)):
+                    word = sentence[i]
+
+                    # Word
+                    if len(list(word)) > config['corpus']['max_word_len']:
+                        reviewID_ls.append(self.word_to_id[self.config['corpus']['unknown_word']])
+                    else:
+                        reviewID_ls.append(self.word_to_id[word])
+
+                    # Char
+                    if len(list(word)) > self.config['corpus']['max_word_len']:
+                        reviewCharID_ls.append((np.ones(shape=(self.config['corpus']['max_word_len'],), dtype='int32')*self.char_to_id[self.config['corpus']['padding_char']]).tolist())
+                    else:
+                        charID_ls = []
+                        char_ls = list(word)
+                        for char in char_ls:
+                            charID_ls.append(self.char_to_id[char])
+                        # pad char list
+                        if len(charID_ls) < self.config['corpus']['max_word_len']:
+                            shape = (self.config['corpus']['max_word_len'] - len(charID_ls),)
+                            charID_ls.extend((np.ones(shape=shape, dtype='int32') * self.char_to_id[self.config['corpus']['padding_char']]).tolist())
+                        reviewCharID_ls.append(charID_ls)
+            if len(reviewID_ls)<self.config['corpus']['max_review_len']*self.config['corpus']['max_sentence_len']:
+                dim0 = self.config['corpus']['max_review_len']*self.config['corpus']['max_sentence_len'] - len(reviewID_ls)
+                dim1 = self.config['corpus']['max_word_len']
+                reviewID_ls.extend((np.ones(shape=(dim0,),dtype='int32')*self.word_to_id[self.config['corpus']['padding_word']]).tolist())
+                reviewCharID_ls.extend((np.ones(shape=(dim0,dim1),dtype='int32')*self.char_to_id[self.config['corpus']['padding_char']]).tolist())
+            prepared_review_collection.append(reviewID_ls)
+            prepared_char_collection.append(reviewCharID_ls)
+
+            label = label_collection[j]
+            attr_label_ls = []
+            senti_label_ls = []
+            for i in range(len(label)):
+                if label[i] in {1, 0, -1}:
+                    attr_label_ls.append(1)
+                    if label[i] == 1:
+                        senti_label_ls.append([1, 0, 0])
+                    elif label[i] == 0:
+                        senti_label_ls.append([0, 1, 0])
+                    else:
+                        senti_label_ls.append([0, 0, 1])
+                else:
+                    attr_label_ls.append(0)
+                    senti_label_ls.append([0, 0, 0])
+            prepared_attr_label_collection.append(attr_label_ls)
+            prepared_senti_label_collection.append(senti_label_ls)
+        return prepared_review_collection, prepared_attr_label_collection, prepared_senti_label_collection, prepared_char_collection
 
     def load_training_data(self):
         self.train_review_collection,self.train_attr_label_collection,self.train_senti_label_collection,self.train_char_collection= \
@@ -150,10 +233,10 @@ if __name__ == "__main__":
                         'max_sentence_len':None,
                         'max_review_len':None,
                         'old_train_data_path':'/datastore/liu121/sentidata2/data/aic2018_junyu/tenc_merged_train_data_withChar.pkl'},
-              'emb':{'wordEmb_path':'',
-                     'charEmb_path':''},
-              'training_data':{'train_path':'',
-                               'dev_path':''}}
+              'emb':{'wordEmb_path':'/datastore/liu121/wordEmb/aic2018cwe_wordEmb.pkl',
+                     'charEmb_path':'/datastore/liu121/charEmb/aic2018cwe_charEmb.pkl'},
+              'training_data':{'train_path':'/datastore/liu121/sentidata2/data/aic2018_junyu/merged_train_cwe.pkl',
+                               'dev_path':'/datastore/liu121/sentidata2/data/aic2018_junyu/merged_dev_cwe.pkl'}}
     gen=GenDataGloVeCWE(config)
     gen.stats()
     # gen.prepare_data()
