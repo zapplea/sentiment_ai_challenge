5e306e948a9ca60a6884669b713d1799a1319e17
yibing <zapplea@gmail.com>
2019-03-19 13:29:04 +0800

    add

--- sbatch/flow.sh
+++ sbatch/flow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sbatch run_base_withCharEmb.sh 0 1
-sbatch run_base_withCharEmb.sh 1 1
-sbatch run_base_withCharEmb.sh 2 1
+sbatch run_base_withCharEmb.sh 0 1 cwep
+sbatch run_base_withCharEmb.sh 1 1 cwep
+sbatch run_base_withCharEmb.sh 2 1 cwep
